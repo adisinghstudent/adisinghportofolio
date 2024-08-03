5c503e0db55a3870e0f19bd81caaa21e64f3bdc8
Adi Singh <adis@ntnu.no>
2024-08-02 23:26:56 -0400

    entry

---
+++ src/app/projects/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/* <div className="fixed inset-0 z-0">
+  <img className="absolute inset-0 w-full h-full object-cover opacity-10" src="https://media.giphy.com/media/26BRKEDGBicmow252/giphy.gif" alt="homegif" />
+</div> */}
+
+<div className="relative z-10 flex justify-center transform translate-y-32 h-screen">
+  <h1 className="text-[20rem] font-bold">
+    Projects!!
+  </h1>
+</div>
+</main>
+  );
+}
